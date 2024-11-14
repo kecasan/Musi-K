0ba08b1ceb62e8a299c2ba4d4008b18cbde68931
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-11-13 21:21:35 -0300

    llll

--- Protótipo/js/app.js
+++ Protótipo/js/app.js
@@ -1,4 +1,86 @@
-// Cria a lista para armazenar os tópicos dos módulos da página inicial
+/**
+ * MENU SECUNDÁRIO
+ */
+ var menuDoc = document.getElementsByClassName('link-nav-arrow')[0];
+ var listSecond = document.querySelector('.list-nav-second');
+ 
+ var teste = listSecond.classList;
+ 
+ // console.log(teste);
+ 
+ menuDoc.addEventListener('click', function(){
+ 
+     listSecond.classList.toggle('hide');
+     menuDoc.classList.toggle('arrow');
+     
+ });
+ 
+ /**
+  * MENU HAMBURGUER
+  * ACIONAMENTO DO MENU
+  */
+ 
+  var iconMenu = document.querySelector(".icon-menu");
+ //  var menu = document.querySelector(".menu");
+  var content = document.querySelector(".content");
+  
+  let body = document.querySelector("body");
+ 
+  iconMenu.addEventListener("click", function(){
+ 
+     body.classList.toggle("__move");
+     // menu.classList.toggle('menu__move');
+     // content.classList.toggle('content__move');
+     
+  });
+ 
+ 
+ /*
+  RECOLHIMENTO DO MENU
+  CLICANDO NO CONTENT
+ */
+ 
+ content.addEventListener("click",function(){
+     body.classList.remove("__move");
+ })
+ 
+ 
+ /*
+ MENU ATIVO - INSERINDO A CLASS ACTIVE
+ */
+ 
+ let linkNavs = document.querySelectorAll(".link-nav");
+ 
+ // console.log(linkNavs);
+ 
+ linkNavs.forEach(function(valorCorrente, index, array){
+ 
+     valorCorrente.addEventListener("click", function(){
+         
+         // linkNavs.forEach(function(elemento){
+ 
+         //     elemento.classList.remove("active");
+         // });
+ 
+         // linkNavs.forEach((elemento)=>{
+         //     elemento.classList.remove("active");
+         // });
+ 
+ 
+         // console.log(linkNavs.length);
+ 
+         for(let i = 0; i < linkNavs.length; i++){
+             array[i].classList.remove("active");
+         }
+ 
+         valorCorrente.classList.add("active");
+     });
+ });
+ 
+ 
+ 
+ 
+ // Cria a lista para armazenar os tópicos dos módulos da página inicial
 const modules = [
     {
         level: "basic",
@@ -109,7 +191,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 //Criando pop-up com conteúdo do tópico
-/*function createModal(topic, level) {
+function createModal(topic, level) {
     const modalContent = {
         basic: {
             description: "Aprenda os conceitos fundamentais que servirão como base para toda sua jornada musical.",
@@ -149,10 +231,7 @@ document.addEventListener('DOMContentLoaded', () => {
     `;
 
     document.body.insertAdjacentHTML('beforeend', modalHTML);
-}*/
-
-
-
+}
 
 
 //Ao concluir a atividade o estilo do tópico é alterado
