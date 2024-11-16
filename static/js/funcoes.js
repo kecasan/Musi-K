555d00510ed7d575d027c01a812b5b6df8f74669
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-11-16 04:08:58 -0300

    navbar

--- static/js/funcoes.js
+++ static/js/funcoes.js
@@ -236,3 +236,18 @@ function createModules() {
     
     updateTopicClickHandler();
 }
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.opacity = "0.4";
+}
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "black";
+    document.body.style.opacity = "1";
+}
